5ba2be4f780be13d58b6a39dc05943801d98aaed
n_tananh <tananh691@gmail.com>
2021-11-16 10:08:18 +0700

    Add project structure for FE and BE

---
+++ asset-management-be/src/test/java/com/nt/rookies/asset/management/AssetManagementBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt.rookies.asset.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagementBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
