9530a2724702a20cc14b7fb5faceccf16ebfbd79
Phuong Ngo <phuongngoforwork@gmail.com>
2021-11-17 14:04:24 +0700

    Back-end: finalize entities and DTOs

--- asset-management-be/src/main/java/com/nt/rookies/asset/management/entity/Asset.java
+++ asset-management-be/src/main/java/com/nt/rookies/asset/management/entity/Asset.java
@@ -1,10 +1,16 @@
 package com.nt.rookies.asset.management.entity;
 
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.persistence.*;
-import java.util.Date;
 
 @Getter
 @Setter
--- asset-management-be/src/main/java/com/nt/rookies/asset/management/entity/Assignment.java
+++ asset-management-be/src/main/java/com/nt/rookies/asset/management/entity/Assignment.java
@@ -1,11 +1,17 @@
 package com.nt.rookies.asset.management.entity;
 
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
-import javax.persistence.*;
-import java.util.Date;
 
 @Setter
 @Getter
