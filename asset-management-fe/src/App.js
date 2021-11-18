72f0f472f139202d612762ff0f570ccedca3e42b
TQH <tranhung99nt@gmail.com>
2021-11-18 20:47:00 +0700

    [#243FE] modularize Sidebar

--- asset-management-fe/src/App.js
+++ asset-management-fe/src/App.js
@@ -14,16 +14,13 @@ import ManageAsset from "./pages/ManageAsset/ManageAsset";
 import ManageUser from "./pages/ManageUser/ManageUser";
 import Report from "./pages/Report/Report";
 import Header from "./components/Header/Header";
-const headerTitle = {
-    Home: 'Home',
-    User: 'Manage User',
-    Asset: 'Manage Asset',
-    Assignment: 'Manage Assignment',
-    Request: 'Request Of Returning',
-    Report: 'Report',
-}
+import Sidebar from "./components/Sidebar/Sidebar";
+
 export default function App() {
-    const [headerInfo, setHeaderInfo] = useState(headerTitle.Home);
+    const [headerInfo, setHeaderInfo] = useState("");
+    const onChanceSidebar = (itemClicked) => {
+        setHeaderInfo(itemClicked);
+    }
     return (
         <Router>
             <div>
@@ -31,32 +28,7 @@ export default function App() {
                 <div className="appcontainer">
                     <div className="grid wide">
                         <div className="row app-content">
-                            <div className="col col-lg-3 col-md-4 col-sm-2 ">
-                                <img className="logo-img" src={logoimg} />
-                                <div className="app-content__title">Online Asset Management</div>
-                                <nav className="category">
-                                    <ul className="category-list">
-                                        <li className="category-item" onClick={() => setHeaderInfo(headerTitle.Home)}>
-                                            <NavLink exact activeClassName="selected" className="category-item__link" to="/">Home</NavLink>
-                                        </li>
-                                        <li  onClick={() => setHeaderInfo(headerTitle.User)}>
-                                            <NavLink  exact activeClassName="selected" className="category-item__link" to="/user">Manage User</NavLink>
-                                        </li>
-                                        <li className="category-item" onClick={() => setHeaderInfo(headerTitle.Asset)}>
-                                            <NavLink exact activeClassName="selected" className="category-item__link" to="/asset">Manage Asset</NavLink>
-                                        </li>
-                                        <li className="category-item" onClick={() => setHeaderInfo(headerTitle.Assignment)}>
-                                            <NavLink activeClassName="selected" className="category-item__link" to="/assignment">Manage Assignment</NavLink>
-                                        </li>
-                                        <li className="category-item" onClick={() => setHeaderInfo(headerTitle.Request)}>
-                                            <NavLink activeClassName="selected" className="category-item__link" to="/requestofreturning">Request Of Returning</NavLink>
-                                        </li>
-                                        <li className="category-item" onClick={() => setHeaderInfo(headerTitle.Report)}>
-                                            <NavLink activeClassName="selected" className="category-item__link" to="/report">Report</NavLink>
-                                        </li>
-                                    </ul>
-                                </nav>
-                            </div>
+                            <Sidebar onChanceSidebar={onChanceSidebar}/>
                             <div className="col col-lg-9 col-md-8 col-sm-10">
                                 <Switch>
                                     <Route path="/" exact>
--- asset-management-fe/src/components/Header/Header.js
+++ asset-management-fe/src/components/Header/Header.js
@@ -2,11 +2,23 @@ import '../../assets/styles/custom.css'
 import '../../assets/styles/main.css'
 const Header = ({header}) =>{
     return (
-        <header className="header">
-            <div className="grid wide">
-                <span className="headerinfo">{header}</span>
+        <nav className="header navbar navbar-expand-md">
+            <div className="container-fluid">
+                <div className="grid wide">
+                    <span className="headerinfo">{header}</span>
+                </div>
+                <div>
+                    <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Login</a>
+                        </li>
+                        <li>
+                            <a className="nav-link" href="#">Logout</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </header>
+        </nav>
     )
 }
 export  default Header
