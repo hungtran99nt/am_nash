bbb689c86a0c7e2bbc0ba1c125237b4d02131c63
TQH <tranhung99nt@gmail.com>
2021-11-19 09:47:01 +0700

    [#243FE] return old sidebar :(

--- asset-management-fe/src/App.js
+++ asset-management-fe/src/App.js
@@ -14,17 +14,23 @@ import ManageAsset from "./pages/ManageAsset/ManageAsset";
 import ManageUser from "./pages/ManageUser/ManageUser";
 import Report from "./pages/Report/Report";
 import Header from "./components/Header/Header";
-import Sidebar from "./components/Sidebar/Sidebar";
 import Login from "./pages/Login/Login";
 import axios from "axios";
+
+const headerTitle = {
+    Home: 'Home',
+    User: 'Manage User',
+    Asset: 'Manage Asset',
+    Assignment: 'Manage Assignment',
+    Request: 'Request Of Returning',
+    Report: 'Report',
+}
 
 export default function App() {
     const [headerInfo, setHeaderInfo] = useState("");
     const [user, setUser] = useState({});
     const [token, setToken] = useState(localStorage.getItem("TOKEN"));
-    const onChanceSidebar = (itemClicked) => {
-        setHeaderInfo(itemClicked);
-    }
+
     let curUsername = localStorage.getItem("USERNAME");
 
     useEffect(() => {
@@ -50,7 +56,32 @@ export default function App() {
                 <div className="appcontainer">
                     <div className="grid wide">
                         <div className="row app-content">
-                            <Sidebar onChanceSidebar={onChanceSidebar}/>
+                            <div className="col col-lg-3 col-md-4 col-sm-2 ">
+                                <img className="logo-img" src={logoimg} />
+                                <div className="app-content__title">Online Asset Management</div>
+                                <nav className="category">
+                                    <ul className="category-list">
+                                        <li className="category-item" onClick={() => setHeaderInfo(headerTitle.Home)}>
+                                            <NavLink exact activeClassName="selected" className="category-item__link" to="/">Home</NavLink>
+                                        </li>
+                                        <li  onClick={() => setHeaderInfo(headerTitle.User)}>
+                                            <NavLink  exact activeClassName="selected" className="category-item__link" to="/user">Manage User</NavLink>
+                                        </li>
+                                        <li className="category-item" onClick={() => setHeaderInfo(headerTitle.Asset)}>
+                                            <NavLink exact activeClassName="selected" className="category-item__link" to="/asset">Manage Asset</NavLink>
+                                        </li>
+                                        <li className="category-item" onClick={() => setHeaderInfo(headerTitle.Assignment)}>
+                                            <NavLink activeClassName="selected" className="category-item__link" to="/assignment">Manage Assignment</NavLink>
+                                        </li>
+                                        <li className="category-item" onClick={() => setHeaderInfo(headerTitle.Request)}>
+                                            <NavLink activeClassName="selected" className="category-item__link" to="/requestofreturning">Request Of Returning</NavLink>
+                                        </li>
+                                        <li className="category-item" onClick={() => setHeaderInfo(headerTitle.Report)}>
+                                            <NavLink activeClassName="selected" className="category-item__link" to="/report">Report</NavLink>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
                             <div className="col col-lg-9 col-md-8 col-sm-10">
                                 <Switch>
                                     <Route path="/" exact>
