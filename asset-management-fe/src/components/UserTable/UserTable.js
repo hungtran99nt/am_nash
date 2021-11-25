4a9a6c54582fdd6b98daf78979f1bfe1ca87e0f0
hqdao.lawvnu <hqdao.lawvnu@gmail.com>
2021-11-25 22:52:21 +0700

    Format code again

--- asset-management-fe/src/components/UserTable/UserTable.js
+++ asset-management-fe/src/components/UserTable/UserTable.js
@@ -1,189 +1,177 @@
-import BootstrapTable from "react-bootstrap-table-next";
-import React, { useEffect, useState } from "react";
+import BootstrapTable from 'react-bootstrap-table-next';
+import React, {useEffect, useState} from "react";
 import UserPopup from "./UserModal/UserPopup";
-import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css";
-import { API_URL, DATE_FORMAT, SORT_ORDERS } from "../../common/constants";
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
+import {API_URL, DATE_FORMAT, SORT_ORDERS} from "../../common/constants";
 import axios from "axios";
-import editImg from "../../assets/images/pen.png";
-import deleteImg from "../../assets/images/cross.png";
-import "./UserTable.css";
-import { useHistory } from "react-router-dom";
+import editImg from '../../assets/images/pen.png'
+import deleteImg from '../../assets/images/cross.png'
+import './UserTable.css'
+import {useHistory} from "react-router-dom";
 import moment from "moment";
-import { pagination, sortCode } from "../../common/config";
+import {pagination, sortCode} from "../../common/config";
 import UserDisableError from "./UserModal/UserDisableError";
 import UserDisableConfirmation from "./UserModal/UserDisableConfirmation";
 
-const defaultSorted = [
-  {
-    dataField: "staffCode",
-    order: SORT_ORDERS.ASC,
-  },
-];
+const defaultSorted = [{
+	dataField: 'staffCode',
+	order: SORT_ORDERS.ASC
+}]
 
-const UserTable = ({ users, isLoading, isRecentUser }) => {
-  const handleEditClicked = (id) => {
-    history.push(`/edit/${id}`);
-  };
-  const [showErr, setShowErr] = useState(false);
-  const handleCloseErr = () => setShowErr(false);
-  const handleShowErr = () => setShowErr(true);
 
-  const [showConfirm, setShowConfirm] = useState(false);
-  const handleCloseConfirm = () => setShowConfirm(false);
-  const handleShowConfirm = () => setShowConfirm(true);
-  const [disableUser, setDisableUser] = useState(false);
-  const [idDisable, setIdDisable] = useState(null);
+const UserTable = ({users, isLoading, isRecentUser}) => {
 
-  const handleDeleteClicked = (id) => {
-    axios
-      .get(`${API_URL}/users/${id}/valid`)
-      .then((res) => {
-        setIdDisable(id);
-        handleShowConfirm();
-      })
-      .catch((err) => {
-        handleShowErr();
-      });
-  };
+	const handleEditClicked = id => {
+		history.push(`/edit/${id}`);
+	}
 
-  const columnFormatter = (cell, row) => {
-    return (
-      <div className="table__actions">
-        <span
-          className="action__items"
-          title={`Edit user ${row.userName}`}
-          onClick={() => handleEditClicked(row.id)}
-        >
-          <img src={editImg} alt="edit" />
-        </span>
+	const [showErr, setShowErr] = useState(false);
+	const handleCloseErr = () => setShowErr(false);
+	const handleShowErr = () => setShowErr(true);
 
-        <span
-          className="action__items"
-          title={`Delete user ${row.userName}`}
-          onClick={() => handleDeleteClicked(row.id)}
-        >
-          <img src={deleteImg} alt="delete" />
-        </span>
-      </div>
-    );
-  };
+	const [showConfirm, setShowConfirm] = useState(false);
+	const handleCloseConfirm = () => setShowConfirm(false);
+	const handleShowConfirm = () => setShowConfirm(true);
+	const [disableUser, setDisableUser] = useState(false);
+	const [idDisable, setIdDisable] = useState(null);
 
-  const columns = [
-    {
-      dataField: "staffCode",
-      text: "Staff Code",
-      sort: true,
-      sortFunc: (a, b, order) => {
-        return sortCode(a, b, order);
-      },
-    },
-    {
-      dataField: "fullName",
-      text: "Full Name",
-      sort: true,
-    },
-    {
-      dataField: "userName",
-      text: "Username",
-    },
-    {
-      dataField: "joinedDate",
-      text: "Join Date",
-      sort: true,
-      sortFunc: (a, b, order) => {
-        if (order === SORT_ORDERS.ASC)
-          return moment(a, DATE_FORMAT.TO) - moment(b, DATE_FORMAT.TO);
-        return moment(b, DATE_FORMAT.TO) - moment(a, DATE_FORMAT.TO);
-      },
-    },
-    {
-      dataField: "type",
-      text: "Type",
-      sort: true,
-    },
-    {
-      dataField: "action",
-      text: "",
-      width: "50",
-      events: {
-        onClick: (e) => {
-          e.stopPropagation();
-        },
-      },
-      formatter: columnFormatter,
-      headerStyle: () => {
-        return { width: "100px" };
-      },
-    },
-  ];
+	const handleDeleteClicked = (id) => {
+		axios
+		.get(`${API_URL}/users/${id}/valid`)
+		.then((res) => {
+			setIdDisable(id);
+			handleShowConfirm();
+		})
+		.catch((err) => {
+			handleShowErr();
+		});
+	};
 
-  const [userDetail, setUserDetail] = useState({});
-  const [userIdPopup, setUserIdPopup] = useState(1);
+	const columnFormatter = (cell, row) => {
+		return (
+			<div className="table__actions">
+			<span
+				className="action__items"
+				title={`Edit user ${row.userName}`}
+				onClick={() => handleEditClicked(row.id)}
+			>
+				<img src={editImg} alt="edit"/>
+			</span>
 
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  let history = useHistory();
+				<span
+					className="action__items"
+					title={`Delete user ${row.userName}`}
+					onClick={() => handleDeleteClicked(row.id)}
+				>
+				<img src={deleteImg} alt="delete"/>
+			</span>
+			</div>
+		)
+	};
 
-  // Get user detail for popup
-  useEffect(() => {
-    axios({
-      method: "GET",
-      url: `${API_URL}/users/${userIdPopup}`,
-    })
-      .then((res) => {
-        setUserDetail(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [userIdPopup]);
+	const columns = [
+		{
+			dataField: 'staffCode',
+			text: 'Staff Code',
+			sort: true,
+			sortFunc: (a, b, order) => {
+				return sortCode(a, b, order);
+			}
+		}, {
+			dataField: 'fullName',
+			text: 'Full Name',
+			sort: true
+		}, {
+			dataField: 'userName',
+			text: 'Username',
+		}, {
+			dataField: 'joinedDate',
+			text: 'Join Date',
+			sort: true,
+			sortFunc: (a, b, order) => {
+				if (order === SORT_ORDERS.ASC)
+					return moment(a, DATE_FORMAT.TO) - moment(b, DATE_FORMAT.TO);
+				return moment(b, DATE_FORMAT.TO) - moment(a, DATE_FORMAT.TO);
+			}
+		}, {
+			dataField: 'type',
+			text: 'Type',
+			sort: true
+		}, {
+			dataField: 'action',
+			text: '',
+			width: '50',
+			events: {
+				onClick: (e) => {
+					e.stopPropagation();
+				}
+			},
+			formatter: columnFormatter,
+			headerStyle: () => {
+				return {width: '100px'};
+			}
+		}
+	];
 
-  const getUserDetail = {
-    onClick: (e, row) => {
-      setUserIdPopup(row.id);
-      toggleTrueFalse();
-    },
-  };
+	const [userDetail, setUserDetail] = useState({});
+	const [userIdPopup, setUserIdPopup] = useState(1);
 
-  const toggleTrueFalse = () => {
-    setShow(handleShow);
-  };
+	const [show, setShow] = useState(false);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+	let history = useHistory();
 
-  return (
-    <>
-      <BootstrapTable
-        keyField="id"
-        data={users}
-        columns={columns}
-        hover
-        rowEvents={getUserDetail}
-        formatter={columnFormatter}
-        defaultSorted={isRecentUser ? [] : defaultSorted}
-        pagination={pagination}
-      />
-      {isLoading && <div>Loading...</div>}
-      {show ? (
-        <UserPopup
-          show={show}
-          handleClose={handleClose}
-          userInfo={userDetail}
-        />
-      ) : null}
-      {showErr ? (
-        <UserDisableError showErr={showErr} handleCloseErr={handleCloseErr} />
-      ) : null}
+	// Get user detail for popup
+	useEffect(() => {
+		axios({
+			method: 'GET',
+			url: `${API_URL}/users/${userIdPopup}`
+		}).then(res => {
+			setUserDetail(res.data);
+		}).catch(err => {
+			console.log(err);
+		})
+	}, [userIdPopup])
 
-      {showConfirm ? (
-        <UserDisableConfirmation
-          idDisable={idDisable}
-          showConfirm={showConfirm}
-          handleCloseConfirm={handleCloseConfirm}
-          setDisableUser={setDisableUser}
-        />
-      ) : null}
-    </>
-  );
-};
+	const getUserDetail = {
+		onClick: (e, row) => {
+			setUserIdPopup(row.id);
+			toggleTrueFalse();
+		},
+	}
+
+	const toggleTrueFalse = () => {
+		setShow(handleShow);
+	};
+
+	return (
+		<>
+			<BootstrapTable
+				keyField='id'
+				data={users}
+				columns={columns}
+				hover
+				rowEvents={getUserDetail}
+				formatter={columnFormatter}
+				defaultSorted={isRecentUser ? [] : defaultSorted}
+				pagination={pagination}
+			/>
+			{isLoading && <div>Loading...</div>}
+			{show ? <UserPopup show={show} handleClose={handleClose} userInfo={userDetail}/> : null}
+			{showErr ? (
+				<UserDisableError showErr={showErr} handleCloseErr={handleCloseErr} />
+			) : null}
+
+			{showConfirm ? (
+				<UserDisableConfirmation
+				idDisable={idDisable}
+				showConfirm={showConfirm}
+				handleCloseConfirm={handleCloseConfirm}
+				setDisableUser={setDisableUser}
+				/>
+			) : null}
+		</>
+	)
+}
 
 export default UserTable;
