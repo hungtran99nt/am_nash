341e71a186c540d915870ab0400d2ee752565d43
hqdao.lawvnu <hqdao.lawvnu@gmail.com>
2021-12-01 19:09:30 +0700

    Completed migration

--- asset-management-fe/src/components/AssignmentTable/ManageAssignmentAction.js
+++ asset-management-fe/src/components/AssignmentTable/ManageAssignmentAction.js
@@ -2,32 +2,16 @@ import React,  {useState} from 'react';
 import {useHistory} from "react-router-dom";
 import {FILTER_ASM_STATE_OPTIONS} from "../../common/constants";
 import {BsPencilFill, FaRegTimesCircle, FaUndoAlt} from "react-icons/all";
-import axios from 'axios';
-import { API_URL } from "../../common/constants";
-import AssignmentDeleteConfirmation from './AssignmentModal/AssignmentDeleteConfirmation'
 
-const ManageAssignmentAction = ({cell, row, assignments}) => {
+
+const ManageAssignmentAction = ({cell, row, handleDeleteClicked}) => {
     const history = useHistory();
 
     const handleEditClicked = (id) => {
         history.push(`edit/assignment/${id}`)
     }
-   
-	const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
-	const [idDelete, setIdDelete] = useState(null);
-	const handleCloseDeleteConfirm = () => setShowDeleteConfirm(false);
-	const handleShowDeleteConfirm = () => setShowDeleteConfirm(true);
-
-    const handleDeleteClicked = (id) => {
-		setIdDelete(id);
-		axios.get(`${API_URL}/admin/assignments/${id}/valid`).then((response) => {
-			if (response.data === true) {
-				handleShowDeleteConfirm();
-			}
-			
-		}).catch(err => {alert(`Error with check valid to delete asset ${err}`)})
-	}
-
+    
+	
     return (
         <div className='table__actions'>
             {/* Edit button: only available when assignment is waiting for accept */}
@@ -49,7 +33,7 @@ const ManageAssignmentAction = ({cell, row, assignments}) => {
                 onClick={
                     row.state !== FILTER_ASM_STATE_OPTIONS.ACCEPTED
                     && row.state !== FILTER_ASM_STATE_OPTIONS.WAITING_FOR_RETURNING ?
-                        () => {handleDeleteClicked(row.id)} : undefined
+                        () => handleDeleteClicked(row.id) : undefined
                 }
                 title={"Delete assignment"}
             />
@@ -66,15 +50,6 @@ const ManageAssignmentAction = ({cell, row, assignments}) => {
                 }
                 title={"Return assignment"}
             />
-            {
-				showDeleteConfirm &&
-				<AssignmentDeleteConfirmation
-					showDeleteConfirm={showDeleteConfirm}
-					handleCloseDeleteConfirm={handleCloseDeleteConfirm}
-					assignments={assignments}
-					idDelete={idDelete}
-				/>
-			}
         </div>
     );
 };
