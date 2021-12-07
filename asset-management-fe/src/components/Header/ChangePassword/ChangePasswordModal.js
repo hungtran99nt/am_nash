b4210c539f63bf1d03ac248b34ffe332fda2da25
TQH <tranhung99nt@gmail.com>
2021-12-08 01:01:26 +0700

    [#242] update form validation, add some css

--- asset-management-fe/src/components/Header/ChangePassword/ChangePasswordModal.js
+++ asset-management-fe/src/components/Header/ChangePassword/ChangePasswordModal.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
-import {Button, Col, Form, Modal, Row} from "react-bootstrap";
+import {Button, Col, Form, InputGroup, Modal, Row} from "react-bootstrap";
 import * as Yup from 'yup';
 import {useFormik} from 'formik';
 import axios from "axios";
 import {API_URL} from "../../../common/constants";
+import {BsFillEyeSlashFill, BsFillEyeFill} from "react-icons/all";
+import './ChangePasswordModal.css'
 
 const Schema = Yup.object().shape({
     oldPassword: Yup.string().required("Required"),
@@ -42,14 +44,19 @@ const ChangePasswordModal = ({show, handleClose}) => {
         }).catch(() => {
             setErrors({oldPassword: "Password is incorrect"})
         });
-        
     }
 
     const formik = useFormik({
         initialValues: initialValues,
         onSubmit: submit,
-        validationSchema: Schema
+        validationSchema: Schema,
     });
+
+    const [isHided, setIsHided] = useState({
+        oldPassword: true,
+        newPassword: true,
+        confirmPassword: true
+    })
 
     return (
         <Modal
@@ -72,62 +79,106 @@ const ChangePasswordModal = ({show, handleClose}) => {
             {!isSubmitted &&
                 <Modal.Body>
                     <form onSubmit={formik.handleSubmit}>
-                        <Form.Group as={Row} className="mb-3" controlId="formOldPassword">
+                        <Form.Group as={Row} className="mb-3">
                             <Form.Label column sm="4">Old password</Form.Label>
                             <Col sm="6">
-                                <Form.Control
-                                    type="password"
-                                    name="oldPassword"
-                                    onBlur={formik.handleBlur}
-                                    onChange={formik.handleChange}
-                                    value={formik.values.oldPassword}
-                                    isInvalid={formik.touched.oldPassword && formik.errors.oldPassword}
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    {formik.errors.oldPassword}
-                                </Form.Control.Feedback>
+                                <InputGroup id="input-group-header">
+                                    <Form.Control
+                                        id="formPass"
+                                        type={isHided.oldPassword ? "password" : "text"}
+                                        name="oldPassword"
+                                        onBlur={formik.handleBlur}
+                                        onChange={formik.handleChange}
+                                        value={formik.values.oldPassword}
+                                        isInvalid={formik.touched.oldPassword && formik.errors.oldPassword}
+                                    />
+                                    <Button variant="outline-secondary" id="eye-addon"
+                                            onClick={() => {
+                                                setIsHided(prevState => ({
+                                                    ...prevState,
+                                                    oldPassword: !isHided.oldPassword
+                                                }));
+                                            }}
+                                    >
+                                        {isHided.oldPassword ? <BsFillEyeFill/> : <BsFillEyeSlashFill/>}
+                                    </Button>
+                                    <Form.Control.Feedback type="invalid">
+                                        {formik.errors.oldPassword}
+                                    </Form.Control.Feedback>
+                                </InputGroup>
                             </Col>
                         </Form.Group>
-                        <Form.Group as={Row} className="mb-3" controlId="formNewPassword">
+                        <Form.Group as={Row} className="mb-3">
                             <Form.Label column sm="4">New password</Form.Label>
                             <Col sm="6">
-                                <Form.Control
-                                    type="password"
-                                    name="newPassword"
-                                    onBlur={formik.handleBlur}
-                                    onChange={formik.handleChange}
-                                    value={formik.values.newPassword}
-                                    isInvalid={formik.touched.newPassword && formik.errors.newPassword}
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    {formik.errors.newPassword}
-                                </Form.Control.Feedback>
+                                <InputGroup id="input-group-header">
+                                    <Form.Control
+                                        id="formPass"
+                                        type={isHided.newPassword ? "password" : "text"}
+                                        name="newPassword"
+                                        onBlur={formik.handleBlur}
+                                        onChange={formik.handleChange}
+                                        value={formik.values.newPassword}
+                                        isInvalid={formik.touched.newPassword && formik.errors.newPassword}
+                                    />
+                                    <Button variant="outline-secondary" id="eye-addon"
+                                            onClick={() => {
+                                                setIsHided(prevState => ({
+                                                    ...prevState,
+                                                    newPassword: !isHided.newPassword
+                                                }));
+                                            }}
+                                    >
+                                        {isHided.newPassword ? <BsFillEyeFill/> : <BsFillEyeSlashFill/>}
+                                    </Button>
+                                    <Form.Control.Feedback type="invalid">
+                                        {formik.errors.newPassword}
+                                    </Form.Control.Feedback>
+                                </InputGroup>
                             </Col>
                         </Form.Group>
-                        <Form.Group as={Row} className="mb-3" controlId="formComfirmPassword">
+                        <Form.Group as={Row} className="mb-3">
                             <Form.Label column sm="4">Confirm password</Form.Label>
                             <Col sm="6">
-                                <Form.Control
-                                    type="password"
-                                    name="confirmPassword"
-                                    onBlur={formik.handleBlur}
-                                    onChange={formik.handleChange}
-                                    value={formik.values.confirmPassword}
-                                    isInvalid={formik.touched.confirmPassword && formik.errors.confirmPassword}
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    {formik.errors.confirmPassword}
-                                </Form.Control.Feedback>
+                                <InputGroup id="input-group-header">
+                                    <Form.Control
+                                        id="formPass"
+                                        type={isHided.confirmPassword ? "password" : "text"}
+                                        name="confirmPassword"
+                                        onBlur={formik.handleBlur}
+                                        onChange={formik.handleChange}
+                                        value={formik.values.confirmPassword}
+                                        isInvalid={formik.touched.confirmPassword && formik.errors.confirmPassword}
+                                    />
+                                    <Button variant="outline-secondary" id="eye-addon"
+                                            onClick={() => {
+                                                setIsHided(prevState => ({
+                                                    ...prevState,
+                                                    confirmPassword: !isHided.confirmPassword
+                                                }));
+                                            }}
+                                    >
+                                        {isHided.confirmPassword ? <BsFillEyeFill/> : <BsFillEyeSlashFill/>}
+                                    </Button>
+                                    <Form.Control.Feedback type="invalid">
+                                        {formik.errors.confirmPassword}
+                                    </Form.Control.Feedback>
+                                </InputGroup>
                             </Col>
                         </Form.Group>
-                        <div className="group-btn col-sm-4">
+                        <div className="col-sm-10 p-1 d-flex justify-content-end">
                             <Button type="submit" className="btn-primary"
                                     disabled={!formik.values.oldPassword || !formik.values.newPassword ||
                                         !formik.values.confirmPassword || formik.errors.confirmPassword}
                             >
                                 Save
                             </Button>
-                            <Button className="btn-cancel" type="reset" onClick={handleClose}>
+                            <Button className="btn-cancel" type="reset"
+                                    onClick={() => {
+                                        formik.resetForm();
+                                        handleClose();
+                                    }}
+                            >
                                 Cancel
                             </Button>
                         </div>
@@ -140,12 +191,13 @@ const ChangePasswordModal = ({show, handleClose}) => {
                         Your password has been changed successfully!
                     </Modal.Body>
                     <Modal.Footer className="confirm">
-                        <Button variant="outline-secondary"
+                        <Button variant="outline-secondary" id="cancelPass"
                                 onClick={() => {
+                                    formik.resetForm();
                                     handleClose();
                                     setIsSubmitted(false);
                                 }
-                        }>
+                                }>
                             Cancel
                         </Button>
                     </Modal.Footer>
